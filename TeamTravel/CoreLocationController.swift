9ebf6f808da4b009714b3016a29dc07a2d4056c6
Joseph Hansen <theythemtheir@icloud.com>
2016-10-25 17:41:35 -0600

    created CoreLocationController
    created CoreLocationController and added functions to appDelegate

---
+++ TeamTravel/CoreLocationController.swift
@@ -0,0 +1,80 @@
+//
+//  CoreLocationController.swift
+//  TeamTravel
+//
+//  Created by Joseph Hansen on 10/25/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class CoreLocationController: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = CoreLocationController()
+    
+    var locationManager: CLLocationManager?
+    
+    var currentTravelerLocation: CLLocation? {
+        didSet{
+            
+        }
+    }
+    
+    
+    
+    var hasAccess: Bool {
+//        locationManager?.requestAlwaysAuthorization()
+        return checkForAuthorizationStatus()
+    }
+    
+    func checkForAuthorizationStatus() -> Bool {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+            return true
+        case .authorizedWhenInUse:
+            return false
+        case .denied:
+            return false
+        case .notDetermined:
+            return false
+        case .restricted:
+            return false
+        }
+        
+    }
+    
+/// sets up locationManager to be delegate, sets accuracy to be within 10 Meters
+    func setupLocationManager() {
+        let locMan = CLLocationManager()
+        locMan.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locMan.distanceFilter = 10
+        locMan.delegate = self
+        self.locationManager = locMan
+        getCurrentLocation()
+    }
+    
+    func getCurrentLocation() {
+        locationManager?.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        DispatchQueue.main.async {
+            NSLog("Got User Location")
+            
+            if let location = locations.first {
+                self.currentTravelerLocation = location
+            }
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog(error.localizedDescription)
+    }
+    
+    
+    
+    
+    
+}

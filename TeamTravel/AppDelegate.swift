4c6d6d8f7254cfd7a596cf242e1627b20c3816f6
Joseph Hansen <theythemtheir@icloud.com>
2016-10-27 17:11:00 -0600

    added multiple view controllers from practice project
    changes to app delegate, more edits to come before pushing to main branch

--- TeamTravel/AppDelegate.swift
+++ TeamTravel/AppDelegate.swift
@@ -44,6 +44,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         CoreLocationController.shared.setupLocationManager()
         self.strongReferenceToLocationManager = CoreLocationController.shared.locationManager
         
+        //Makes NavigationBar Transparent
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+        UINavigationBar.appearance().isTranslucent = true
+        
         return true
     }
 

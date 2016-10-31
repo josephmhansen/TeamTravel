73e92bfbe75ffd153c955d878297c2f0bbc0f3de
Joseph Hansen <theythemtheir@icloud.com>
2016-10-31 12:12:37 -0600

    created segmentedController Externally
    added segmented controller from outside demo project, need to change to three options instead of 4,

---
+++ TeamTravel/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentedStyle.swift
+//  TeamTravel
+//
+//  Created by Joseph Hansen on 10/31/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import UIKit
+
+internal struct SelectionIndicator {
+    static let defaultHeight: CGFloat = 5
+}
---
+++ TeamTravel/SCScrollView.swift
@@ -0,0 +1,48 @@
+//
+//  SCScrollView.swift
+//  TeamTravel
+//
+//  Created by Joseph Hansen on 10/31/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import UIKit
+
+internal class SCScrollView: UIScrollView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isDragging {
+            next?.touchesBegan(touches, with: event)
+        } else {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isDragging {
+            next?.touchesMoved(touches, with: event)
+        } else {
+            super.touchesMoved(touches, with: event)
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isDragging {
+            next?.touchesEnded(touches, with: event)
+        } else {
+            super.touchesEnded(touches, with: event)
+        }
+    }
+}
+
+internal extension SCScrollView {
+    internal var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}

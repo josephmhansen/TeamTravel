623187c5cf25cc70df92734ad87fc56de1997846
AustinDev16 <austin.blaser@gmail.com>
2016-11-12 12:46:12 -0700

    Added a controller to monitor whether a user is within a certain distance of a location.

---
+++ TeamTravel/LocationProximityManager.swift
@@ -0,0 +1,58 @@
+//
+//  LocationProximityManager.swift
+//  TeamTravel
+//
+//  Created by Austin Blaser on 11/12/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+
+class LocationProximityManager {
+    
+    static let shared = LocationProximityManager()
+    
+    // MARK: - Properties
+    /// Monitored locations are tied those locations returned by the search MKSearch.
+    var monitoredLocations: [Location] {
+     return  SearchLocationController.shared.allReturnedLocations
+    }
+    
+    var locationsWithinProximity: [Location] = []
+    
+    // MARK: - Functions
+    
+    func evaluateProximityForMonitoredLocations(){
+        // reset locations before evaluating
+        locationsWithinProximity = []
+        
+        
+    }
+    
+    func didEnterLocations(){
+        for location in locationsWithinProximity {
+            
+            // Add location to Master traveler
+            //TravelerController.shared.addVisited(region: region)
+
+            
+            fireAlertForEnteredRegion(location: location)
+        }
+    }
+    
+    func fireAlertForEnteredRegion(location: Location){
+        let regionAlert = UIAlertController(title: "Entered: \(location.locationName)", message: nil, preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .cancel)
+        regionAlert.addAction(dismiss)
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 4, repeats: false)
+        
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: Notification.Name(rawValue: postAlertNotification), object: regionAlert)
+        }
+        
+        Notifications.sendNotification(withTitle: "You entered \(location.locationName)", andTrigger: trigger)
+    }
+}
